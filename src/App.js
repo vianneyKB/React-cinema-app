9bada4bfb6c5541811dd95f845521e202143c65b
vianney <vianney.kb.723@gmail.com>
2021-04-13 23:12:27 +0200

    fixing linting bugs

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import React from "react";
-import { Provider } from "react-redux";
+import React from 'react';
+import { Provider } from 'react-redux';
 
-import "./App.css";
-import store from "./redux/store";
+import './App.css';
+import store from './redux/store';
 
 const App = () => {
   return (
--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,10 +1,10 @@
-import { combineReducers } from "redux";
+import { combineReducers } from 'redux';
 
-import errorReducer from "./errorReducer";
-import movieReducer from "./movieReducer";
+import errorReducer from './errorReducer';
+import movieReducer from './movieReducer';
 const rootReducers = combineReducers({
   errors: errorReducer,
-  movies: movieReducer,
+  movies: movieReducer
 });
 
 export default rootReducers;
